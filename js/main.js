7f1f94bdd84c1283349b7a6d0844d30700283136
Paola Ortiz de Zevallos Villanueva <paolaozv@gmail.com>
2016-10-09 18:23:53 -0500

    modificación final

--- js/main.js
+++ js/main.js
@@ -1,45 +1,57 @@
 function validateForm(){
-	/* Escribe tú código aquí */
-	var apellido = document.getElementById("lastname").value;
-	var email = document.getElementById("input-email").value;
-	var contrasena = document.getElementById("input-password").value;
-	var nombre = document.getElementById("name").value;
-	var lista = document.getElementsByClassName("form-control").value;
+    /* Escribe tú código aquí */
+    var nombre = document.getElementById("name");
+    var apellido = document.getElementById("lastname");
+    var correo = document.getElementById("input-email");
+    var contra = document.getElementById("input-password");
+    var tipoBici = document.getElementById("tipo-bici");
 
-    if(nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
-    	alert("Campo obligatorio");
-   	}
-   	else {
-       var m = /^[a-zA-Z]*$/;
-       if(!nombre.search(m)) {
-            console.log("letra")
-            m = /[a-z]/g;
-        }    
-        if(!nombre.search(m)){
-                alert("La primera letra debe empezar con maýuscula")
-        }
-   	} 
+    // Campos obligatorios
+    if (nombre.value.trim().length == 0 ||
+    apellido.value.trim().length == 0 ||
+    correo.value.trim().length == 0 ||
+    contra.value.trim().length == 0 ||
+    tipoBici.value == 0) {
+        alert("Ingrese todos los campos obligatorios");
+    }
 
-   	if(apellido == null || apellido.length == 0 || /^\s+$/.test(apellido)) {
-    	alert("Campo obligatorio");
-   	}
-   	else {
-       var n = /^[a-zA-Z]*$/;
-       if(!apellido.search(n)) {
-            console.log("letra")
-            n = /[a-z]/g;
-        }    
-        if(!apellido.search(n)){
-                alert("La primera letra debe empezar con maýuscula")
-        }
-   	} 
+    // A-Z para nombres y apellidos
+    var regexNombre = /^[a-zñáéíóúü]+$/gi;
+    if(!regexNombre.test(nombre.value)) {
+        alert("Ingresa un nombre válido");
+    }
 
-	if(!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email))) {
-		return alert("Ingresa tu email");
-	} else if( contrasena == null || contrasena.length <= 6 || contrasena == "password" || contrasena == 123456 || contrasena == 098754 || /^\s+$/.test(contrasena) ) {
-		return alert("Ingresa una contraseña válida");
-	} else if( lista.selectedIndex == null || lista.selectedIndex == 0 ) { 
-        return alert("Elegir una opción de  la lista");
+    var regexApellido = /^[a-zñáéíóúü]+$/gi;
+    if(!regexApellido.test(apellido.value)) {
+        alert("Ingresa un apellido válido");
+    }
+
+    // Primera letra mayúscula para los campos nombres y apellidos
+    if(nombre.value[0] == nombre.value[0].toLowerCase()) {
+        alert("Ingrese la primera letra del nombre en mayúscula");
+    }
+    if(apellido.value[0] == apellido.value[0].toLowerCase()) {
+        alert("Ingrese la primera letra del apellido en mayúscula");
+    }
+
+    // Formato de Correo
+    var regexCorreo = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    if(!regexCorreo.test(correo.value)) {
+        alert("Ingresa un correo válido");
+    }
+
+    // Password de al menos 6 caracteres
+    if(contra.value.length < 6) {
+        alert("Ingresa un password adecuado");
+    }
+
+    // Password no igual a "password", "123456", "098754"
+    if(contra.value == "password" || contra.value == "123456" || contra.value == "098754") {
+        alert("Ingresa un password adecuado");
+    }
+
+    // El valor seleccionado de bicis, debe ser una de las opciones presentadas
+    if(tipoBici.value != "urbana" && tipoBici.value != "treking" && tipoBici.value != "electrica" && tipoBici.value != "estatica") {
+        alert("Escoge el tipo de bici");
     }
 }
-
